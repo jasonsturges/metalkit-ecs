99295b4216ed0748f92dbb76d95deaa98704304e
Jason Sturges <jason@jasonsturges.com>
2026-04-22 22:58:10 -0500

    Initial Commit

---
+++ MetalKit Test/MetalKit_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalKit_TestApp.swift
+//  MetalKit Test
+//
+//  Created by Jason Sturges on 4/22/26.
+//
+
+import SwiftUI
+
+@main
+struct MetalKit_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
