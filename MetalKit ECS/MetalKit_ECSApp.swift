7494f5b98063a7c61c54d0438aa0a043b2504e65
Jason Sturges <jason@jasonsturges.com>
2026-04-25 14:05:58 -0500

    Refactoring

--- MetalKit ECS/MetalKit_ECSApp.swift
+++ MetalKit ECS/MetalKit_ECSApp.swift
@@ -1,6 +1,6 @@
 //
-//  MetalKit_TestApp.swift
-//  MetalKit Test
+//  MetalKit_ECSApp.swift
+//  MetalKit ECS
 //
 //  Created by Jason Sturges on 4/22/26.
 //
@@ -9,7 +9,7 @@ import Metal
 import SwiftUI
 
 @main
-struct MetalKit_TestApp: App {
+struct MetalKit_ECSApp: App {
     private let renderer = Renderer(device: MTLCreateSystemDefaultDevice()!)
 
     var body: some Scene {
